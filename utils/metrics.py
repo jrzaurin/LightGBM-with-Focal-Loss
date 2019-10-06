fdad056c491aacf859f55c971c094b149c8445d7
jrzaurin <jrzaurin@gmail.com>
2019-10-06 19:59:06 +0100

    fixing a minor typo

--- utils/metrics.py
+++ utils/metrics.py
@@ -57,7 +57,7 @@ def focal_loss_lgb(y_pred, dtrain, alpha, gamma):
 	y_true = dtrain.label
 	def fl(x,t):
 		p = 1/(1+np.exp(-x))
-		return -( a*t + (1-a)*(1-t) ) * (( 1 - ( t*p + (1-t)*(1-p)) )**g) * ( t*np.log(p)+(1-t)*np.log(1-p) )
+		return -( a*t + (1-a)*(1-t) ) * (( 1 - ( t*p + (1-t)*(1-p)) )**g) * ( t*np.log(p) + (1-t)*np.log(1-p) )
 	partial_fl = lambda x: fl(x, y_true)
 	grad = derivative(partial_fl, y_pred, n=1, dx=1e-6)
 	hess = derivative(partial_fl, y_pred, n=2, dx=1e-6)
